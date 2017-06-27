8793be67fd743a8d49c229eea87af65d8bcdea16
daghangunay <daghangunay@gmail.com>
2017-06-27 22:30:24 +0300

    add dockerfile && add wait-for sh

--- service/index.js
+++ service/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://ui:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -19,6 +19,7 @@ app.get('/search/quick', searchController);
 clients.run()
     .then(() =>{
     app.listen(3000, () => {
-        console.log('Port 3000!')
+        console.log('UI 8080!');
+        console.log('SERVICE 3000!');
     })
 });
